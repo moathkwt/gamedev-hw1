34a609059a32f84f7568e068bb26764224708cd9
moathkwt <100304331+moathkwt@users.noreply.github.com>
2022-03-08 18:41:05 +0300

    homework 1

---
+++ homework/homework/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hero = 100;
+
+            int health = 100;
+
+
+            int damaged(int downHealth)
+            {
+                return health - downHealth;
+            }
+            int characterHealth = damaged(20);
+            Console.WriteLine(" damaged health : " + characterHealth);
+
+
+            int boost(int upHealth)
+            {
+                return characterHealth + upHealth;
+            }
+            int boostHealth = boost(10);
+
+
+            Console.WriteLine(" boosted health : " + boostHealth);
+
+            string[] HealthArray = { "80", "90" };
+            Console.WriteLine(HealthArray[1]);
+
+            
+
+            {
+                if (characterHealth > boostHealth)
+                {
+                    Console.WriteLine(boostHealth + " is less than " + characterHealth);
+                }
+                else if (boostHealth > characterHealth)
+                {
+                    Console.WriteLine(characterHealth + " is less than " + boostHealth);
+                }
+                
+
+
+            }
+        }
+    }
+}
